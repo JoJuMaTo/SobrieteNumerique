dbc0918e02e45cc0b5a0dc937319feb4620ce715
John Doe <j.donatmagnin@it-students.fr>
2024-05-23 21:00:00 +0200

    Login using x-www-form-urlencoded

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,6 +29,35 @@ class AuthController extends AbstractController
         return $this->json([
             'message' => $message,
 
+        ]);
+    }
+
+    #[Route('/user/login', name: 'api_auth_login_user', methods: ['POST'])]
+    public function userLogin(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): JsonResponse
+    {
+        $username = $request->request->get('username');
+
+        $user = $entityManager->getRepository(User::class)->findOneBy(['username' => $username]);
+        if ($user === null) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $password = $request->request->get('password');
+
+        if (!$passwordHasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(['error' => 'Invalid credentials',
+                'user' => $user->getEmail(),
+                'password' => $password,
+                'check_password' => $passwordHasher->isPasswordValid($user, $password),
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        //$token = $tokenProvider->createToken($user); // somehow create an API token for $user
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),
+            //'token' => $token,
         ]);
     }
 }
