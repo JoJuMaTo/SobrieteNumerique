2fabc5b2c81873d7439d2fe6ee966a6c8c029e6e
JulienDFS29C <ju.roux@it-students.fr>
2024-05-24 23:33:55 +0200

    MaJ classe principale

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {authInterceptor} from "./interceptor/auth.interceptor";
+import {importProvidersFrom} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...appConfig.providers,
+    provideHttpClient(
+      // registering interceptors
+      withInterceptors([authInterceptor])),
+    importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync(),
+
+  ]
+}).catch((err: any) => console.error(err));
