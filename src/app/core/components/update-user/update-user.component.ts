67dd6fd784ce84d8573317c888fdfe5f7182ab9a
JulienDFS29C <ju.roux@it-students.fr>
2024-05-27 14:41:23 +0200

    suppression user

--- src/app/core/components/update-user/update-user.component.ts
+++ src/app/core/components/update-user/update-user.component.ts
@@ -4,12 +4,17 @@ import {User} from "../../models/user";
 import {UserService} from "../../services/user.service";
 import {environment} from "../../../../environments/environment";
 import {map} from "rxjs/operators";
+import {CustomModalComponent} from "../custom-modal/custom-modal.component";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-update-user',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CustomModalComponent,
+    NgIf
   ],
   templateUrl: './update-user.component.html',
   styleUrl: './update-user.component.css'
@@ -17,13 +22,16 @@ import {map} from "rxjs/operators";
 export class UpdateUserComponent implements OnInit{
 
   updateUrl: string = `${environment.apiUrl}/user/update`;
+  deleteUrl: string = `${environment.apiUrl}/user/delete`;
 
 
+  accountRemovalForm!: FormGroup;
   passwordForm!: FormGroup;
   @Input() user!: User;
+  showModal = false;
 
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService) {
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private authService: AuthService) {
   }
 
   ngOnInit() {
@@ -35,11 +43,20 @@ export class UpdateUserComponent implements OnInit{
 
 
     })
+
+    this.accountRemovalForm = this.formBuilder.group({
+
+      email: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      // username: localStorage.getItem('username'),
+
+
+    })
   }
 
 
 
-  onSubmitForm(): void{
+  onSubmitUpdateForm(): void{
 
     const userData = {
       ...this.passwordForm.value,
@@ -53,4 +70,27 @@ export class UpdateUserComponent implements OnInit{
        map(result => console.log(result.toString())),
      ).subscribe();
   }
+
+  onSubmitRemovalForm(): void{
+
+    const userRemovalData = {
+      ...this.accountRemovalForm.value,
+      token: localStorage.getItem('token')
+    };
+    this.userService.deleteUser(userRemovalData).subscribe();
+    this.authService.logout();
+
+  }
+
+  handleConfirmation(result: boolean) {
+    this.showModal = false;
+    if (result) {
+      console.log('User clicked OK');
+      this.onSubmitRemovalForm()
+    } else {
+      console.log('User clicked Cancel');
+    }
+
+  }
+
 }
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -11,6 +11,8 @@ export class UserService {
 
   registerUrl: string = `${environment.apiUrl}/user/register`;
   updateUrl: string = `${environment.apiUrl}/user/update`;
+  deleteUrl: string = `${environment.apiUrl}/user/delete`;
+
 
 
   constructor(private http: HttpClient) {
@@ -20,17 +22,27 @@ export class UserService {
 
     //TODO : définir URL de l'API côté serveur
 
-    return this.http.post<string>(`${this.registerUrl}`,userForm.value, {responseType: 'text' as 'json'});
+    return this.http.post<string>(`${this.registerUrl}`,JSON.stringify(userForm), {responseType: 'text' as 'json'});
   }
 
   updateUserPassword(userData: any): Observable<any> {
 
     console.log("PASSWORD DATA : " + JSON.stringify(userData));
+    let token: string | null = localStorage.getItem('token');
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
     });
+
     return this.http.put<string>(`${this.updateUrl}`,JSON.stringify(userData), {headers: headers, responseType: 'text' as 'json'});
   }
 
+  deleteUser(token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    });
 
+    return this.http.delete<string>(`${this.deleteUrl}`, { headers: headers, responseType: 'text' as 'json' });
+  }
 }
