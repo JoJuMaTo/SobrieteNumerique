22e1745f725918809001c007bcedd4c2b7f0e007
JulienDFS29C <ju.roux@it-students.fr>
2024-05-29 22:27:34 +0200

    gestion quizz - étape 1 trois-quarts

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Question } from '../core/models/question';
-import {NgIf} from "@angular/common";
+import { NgIf } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-question',
@@ -9,21 +10,36 @@ import {NgIf} from "@angular/common";
     NgIf
   ],
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('500ms ease-in-out')
+      ]),
+      transition(':leave', [
+        animate('500ms ease-in-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class QuestionComponent implements OnInit {
 
-  @Input() question: Question;
+  @Input() question!: Question;
+  @Input() selectedAnswer!: string;
+  @Output() answerSelected = new EventEmitter<{ questionId: number, answer: string }>();
 
   constructor() {
     console.log('QuestionComponent constructor called');
-
   }
 
   ngOnInit() {
     console.log('QuestionComponent ngOnInit called');
-    console.log('Question:', this.question);
+    console.log('Question received in child component:', this.question);
   }
 
-  protected readonly Question = Question;
+  selectAnswer(answer: string) {
+    this.answerSelected.emit({ questionId: this.question.id, answer });
+  }
 }
--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {BehaviorSubject, Observable} from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Question } from '../core/models/question';
 import { QuestionService } from '../core/services/question.service';
-import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { QuestionComponent } from '../question/question.component';
 
 @Component({
@@ -11,20 +11,25 @@ import { QuestionComponent } from '../question/question.component';
   standalone: true,
   imports: [
     NgIf,
-    NgFor,
     CommonModule,
     HttpClientModule,
     QuestionComponent
   ],
   templateUrl: './questionnaire.component.html',
   styleUrls: ['./questionnaire.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class QuestionnaireComponent implements OnInit {
 
   isLoading: boolean = true;
   questions$ = new BehaviorSubject<Question[]>([]);
-  questions!: Observable<Question[]> ;
+  questions: Question[] = [];
+  currentQuestion: Question | null = null;
+  selectedAnswers: { [key: number]: string } = {};
+  questionKey: number = 0;
+  animationState: string = '';
+
   constructor(private questionService: QuestionService, private cdr: ChangeDetectorRef) {
     console.log('QuestionnaireComponent constructor called');
   }
@@ -36,7 +41,10 @@ export class QuestionnaireComponent implements OnInit {
   loadQuestions() {
     this.questionService.getQuestions().subscribe({
       next: (questions) => {
+        this.questions = questions;
         this.questions$.next([...questions]);
+        this.currentQuestion = questions[0];  // Set the first question as the current question
+        this.questionKey = this.currentQuestion.id;
         console.log('Questions received:', questions);
         this.isLoading = false;
         this.cdr.detectChanges();
@@ -46,5 +54,41 @@ export class QuestionnaireComponent implements OnInit {
         this.isLoading = false;
       }
     });
+  }
+
+  nextQuestion() {
+    const nextIndex = this.questions.indexOf(this.currentQuestion!) + 1;
+    if (nextIndex < this.questions.length) {
+      this.triggerAnimation();
+      setTimeout(() => {
+        this.currentQuestion = this.questions[nextIndex];
+        this.questionKey = this.currentQuestion.id;
+        this.cdr.detectChanges();
+      }, 500);
+    }
+  }
+
+  previousQuestion() {
+    const prevIndex = this.questions.indexOf(this.currentQuestion!) - 1;
+    if (prevIndex >= 0) {
+      this.triggerAnimation();
+      setTimeout(() => {
+        this.currentQuestion = this.questions[prevIndex];
+        this.questionKey = this.currentQuestion.id;
+        this.cdr.detectChanges();
+      }, 500);
+    }
+  }
+
+  selectAnswer(event: { questionId: number, answer: string }) {
+    this.selectedAnswers[event.questionId] = event.answer;
+  }
+
+  isAnswerSelected(questionId: number, answer: string): boolean {
+    return this.selectedAnswers[questionId] === answer;
+  }
+
+  triggerAnimation() {
+    this.animationState = this.animationState === 'in' ? 'out' : 'in';
   }
 }
